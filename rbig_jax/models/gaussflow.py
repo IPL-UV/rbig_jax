1eefa85d8fff2260e85211619216369383e204bd
jejjohnson <emanjohnson91@gmail.com>
2021-03-18 01:19:08 +0100

    Refactored GaussFlow model. * updated notebook. * working still.

--- rbig_jax/models/gaussflow.py
+++ rbig_jax/models/gaussflow.py
@@ -2,50 +2,61 @@ import objax
 from objax import Module
 from typing import List
 import jax
+from rbig_jax.transforms.base import CompositeTransform
 
 
-class GaussianizationFlow(Module):
+from typing import List
+
+
+class GaussianizationFlow:
     def __init__(
-        self, n_features: int, bijections: List[Module], base_dist: None, generator=None
+        self, bijections: List, base_dist: None,
     ):
-        # create Gaussinization block
-        self.n_features = n_features
-        self.bijections = bijections
+        # create init f bijections
+        self.init_func = CompositeTransform(bijections)
+
+        # create base distribution
         if base_dist is None:
             base_dist = jax.scipy.stats.norm
 
         self.base_dist = base_dist
 
-        if generator is None:
-            generator = objax.random.Generator(123)
-        self.generator = generator
+    def init_params(self, rng, n_features):
 
-    def __call__(self, X):
-        return self.bijections(X)
+        params, forward_transform, inverse_transform = self.init_func(rng, n_features)
+        self.n_features = n_features
+        self.forward_chain = forward_transform
+        self.inverse_chain = inverse_transform
 
-    def transform(self, X):
-        return self.bijections.transform(X)
+        return params
 
-    def inverse_transform(self, X):
-        return self.bijections.inverse(X)
+    def __call__(self, params, X):
 
-    def log_det_jacobian(self, X):
-        _, logabsdet = self.bijections(X)
+        return self.forward_chain(params, X)
+
+    def transform(self, params, X):
+        X, _ = self.forward_chain(params, X)
+        return X
+
+    def inverse_transform(self, params, X):
+        X, _ = self.inverse_chain(params, X)
+        return X
+
+    def log_det_jacobian(self, params, X):
+        _, logabsdet = self.forward_chain(params, X)
         return logabsdet
 
-    def score_samples(self, X):
-        z, logabsdet = self.bijections(X)
+    def score_samples(self, params, X):
+        z, logabsdet = self.forward_chain(params, X)
 
         log_prob = self.base_dist.logpdf(z)
 
         return log_prob.sum(axis=1) + logabsdet
 
-    def score(self, X):
-        return -self.score_samples(X).mean()
+    def score(self, params, X):
+        return -self.score_samples(params, X).mean()
 
-    def sample(self, n_samples: int):
+    def sample(self, rng, params, n_samples: int):
 
-        X_gauss = objax.random.normal(
-            (n_samples, self.n_features), generator=self.generator
-        )
-        return self.inverse_transform(X_gauss)
+        X_gauss = jax.random.normal(rng, shape=(n_samples, self.n_features))
+        return self.inverse_transform(params, X_gauss)
